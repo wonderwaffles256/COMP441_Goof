26931e4ed685b1ac7cd3fc08e07ae039180d8036
wonderwaffles256 <117676091+wonderwaffles256@users.noreply.github.com>
2024-09-24 18:37:33 -0400

    Main Scene Setup
    Added:
    Course 1 and 2 prefabs
    Ball Prefab
    Canvas with text fields
    Main scene manager

---
+++ Goof/Assets/Scripts/ManagerScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class MainSceneManager : MonoBehaviour
+{
+    public TMP_Text courseNumber;
+    public TMP_Text strokeCount;
+    public TMP_Text winMessage;
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
